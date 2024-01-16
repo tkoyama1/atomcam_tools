da084de2e48bdabff9dda00aba4fbf578e5c02cf
takayuki koyama <95458917+tkoyama1@users.noreply.github.com>
2024-01-16 09:08:45 +0000

    Revert "SMBマウント変更"
    This reverts commit c0c9d57bf6a54d9710c8b4b8d7632b52072dfaa0.

--- overlay_rootfs/scripts/mount_cifs.sh
+++ overlay_rootfs/scripts/mount_cifs.sh
@@ -8,18 +8,24 @@ STORAGE_CIFSUSER=$(awk -F "=" '/STORAGE_CIFSUSER *=/ {print $2}' $HACK_INI)
 STORAGE_CIFSPASSWD=$(awk -F "=" '/STORAGE_CIFSPASSWD *=/ {print $2}' $HACK_INI)
 
 if [ "$STORAGE_CIFS" = "on" -o "$STORAGE_CIFS" = "alarm" -o "$STORAGE_CIFS" = "record" ] && [ "$STORAGE_CIFSSERVER" != "" ]; then
-  # mount | grep "$STORAGE_CIFSSERVER" > /dev/null && exit
+  mount | grep "$STORAGE_CIFSSERVER" > /dev/null && exit
   while [ -f $LOCKFILE ] ; do
     sleep 0.5
   done
-  # mount | grep "$STORAGE_CIFSSERVER" > /dev/null && exit
-  umount -f /atom/mnt
+  mount | grep "$STORAGE_CIFSSERVER" > /dev/null && exit
   touch $LOCKFILE
   for VER in 3.0 2.1 2.0
   do
-    if mount -t cifs -ousername=$STORAGE_CIFSUSER,password=$STORAGE_CIFSPASSWD,vers=$VER $STORAGE_CIFSSERVER /atom/mnt ; then
-      rm -f $LOCKFILE
-      exit 0
+    if [ -d /atom ] ; then
+      if mount -t cifs -ousername=$STORAGE_CIFSUSER,password=$STORAGE_CIFSPASSWD,vers=$VER $STORAGE_CIFSSERVER /atom/mnt ; then
+        rm -f $LOCKFILE
+        exit 0
+      fi
+    else
+      if LD_LIBRARY_PATH=/tmp/system/lib:/usr/lib:/usr/lib/samba /tmp/system/lib/ld.so.1 /tmp/system/bin/busybox mount -t cifs -ousername=$STORAGE_CIFSUSER,password=$STORAGE_CIFSPASSWD,vers=$VER $STORAGE_CIFSSERVER /mnt ; then
+        rm -f $LOCKFILE
+        exit 0
+      fi
     fi
   done
   rm -f $LOCKFILE
